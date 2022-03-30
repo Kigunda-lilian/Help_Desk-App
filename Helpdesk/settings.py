c115bdbf358e0132d6a62ac642e826a4ef22c282
segem001 <kipdom001@gmail.com>
2022-03-31 02:34:44 +0300

    Create MyUser model

--- Helpdesk/settings.py
+++ Helpdesk/settings.py
@@ -80,16 +80,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Helpdesk.wsgi.application'
 
+AUTH_USER_MODEL="helpdesk.MyUser"
+
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': ('helpdesk'),
-        'USER': ('moringa'),
-        'PASSWORD':('1234'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
