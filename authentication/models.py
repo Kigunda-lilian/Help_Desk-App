646a621718e92c7a3edc4149c995eec7af3ac1df
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-31 01:06:38 +0300

    create comment model

--- authentication/models.py
+++ authentication/models.py
@@ -8,3 +8,26 @@ class Tag(models.Model):
     stage= models.CharField(max_length=80)
     logical=models.BooleanField(default=True)
     technical=models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.language
+    
+reactions={('Like','Like'),('Unlike','Unlike')}
+    
+class Comments(models.Model):
+    post = models.ForeignKey(Post,related_name="comments" ,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete = models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=255)
+    reply = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+         return '%s - %s' % (self.post.title, self.name)
+    
+    
+
+   
+    
+    
+    
+
