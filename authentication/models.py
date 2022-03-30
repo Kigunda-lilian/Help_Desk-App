032488bf6f8d9f56ec64260fdeba6d491aab4845
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-03-30 23:51:44 +0300

    create Tag model

--- authentication/models.py
+++ authentication/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import User
 import datetime as dt
 
 # Create your models here.
+class Tag(models.Model):
+    language= models.CharField(max_length=50)
+    stage= models.CharField(max_length=80)
+    logical=models.BooleanField(default=True)
+    technical=models.BooleanField(default=False)
