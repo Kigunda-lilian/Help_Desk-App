3ce80bc62be520caf692de26254072c773c8fc25
DKorir <dominic.korir@student.moringaschool.com>
2022-04-01 06:06:16 +0300

    make migrations for the profile model

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -1,4 +1,6 @@
 from re import A
 from django.contrib import admin
 from authentication.models import Account
+from .models import Profile
 admin.site.register(Account)
+admin.site.register(Profile)
