ce5fb5821838723173f64fa82754330b2a89ea5e
DKorir <dominic.korir@student.moringaschool.com>
2022-04-01 05:59:09 +0300

    add field for bio in profile model

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='images/', default='default.png')
-
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
 
 
