42e768f8d897df7d5250f178a5186bc08454ff6b
DKorir <dominic.korir@student.moringaschool.com>
2022-04-01 06:01:57 +0300

    create field for user imported from account model

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from authentication.models import Account
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='images/', default='default.png')
     bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    user = models.OneToOneField(Account, on_delete=models.CASCADE, related_name='profile')
+
 
 
