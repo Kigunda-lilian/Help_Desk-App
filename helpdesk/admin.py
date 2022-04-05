4f29956677cf90115147988ddc36759fe8f843d5
DKorir <dominic.korir@student.moringaschool.com>
2022-04-06 00:48:10 +0300

    push database

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from authentication.models import Account
-from .models import Profile
+from .models import Profile,Comments,Tag,Like,Post
 admin.site.register(Account)
 admin.site.register(Profile)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comments)
