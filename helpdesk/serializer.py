0c1a9442ebe519a5b0506be24fe24537562f0e4a
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-04-10 18:01:45 +0300

    create a profile serializer

--- helpdesk/serializer.py
+++ helpdesk/serializer.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from .models import Profile
+
+# profile serializer
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ("user", "prof_pic ", "bio")
