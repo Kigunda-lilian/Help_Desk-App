6facdc9017f2c4d98b8b716f120d410f343dfddc
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-04-17 22:12:58 +0300

    change db

--- Helpdesk/settings.py
+++ Helpdesk/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'Helpdesk.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': ('desks'),
+        'NAME': ('deskss'),
         'USER': ('moringa'),
         'PASSWORD':('1234'),
     }
