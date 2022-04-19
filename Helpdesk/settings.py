cfb8e944c0d9ba09e4c94fb53419a53f284efd5c
DKorir <dominic.korir@student.moringaschool.com>
2022-04-19 21:48:14 +0300

    remove migrations

--- Helpdesk/settings.py
+++ Helpdesk/settings.py
@@ -104,16 +104,6 @@ WSGI_APPLICATION = 'Helpdesk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': ('deskss'),
-        'USER': ('moringa'),
-        'PASSWORD':('1234'),
-    }
-}
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
